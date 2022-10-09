ed216e9efaa3fdd5e4ca6da5f592eb266708bc17
guaziba <li3424709@gmail.com>
2022-10-10 01:44:26 +0900

    10月10日　lesson 7~9

---
+++ TestWhile.java
@@ -0,0 +1,13 @@
+public class TestWhile {
+	public static void main(String[] arge) {
+		int x= 10;
+		while(x < 20) {
+			System.out.println("x的值是" + x);
+			x++;
+			System.out.println("\n");
+			
+		}
+	}
+	
+	
+}
