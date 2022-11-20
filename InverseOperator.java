e0307fe77be88e20782ef20808180cf873d0de3c
guaziba <li3424709@gmail.com>
2022-11-21 00:48:28 +0900

    bilibli java 教学练习 到74到85  逻辑运算符到 键盘输入

---
+++ InverseOperator.java
@@ -0,0 +1,17 @@
+public class InverseOperator{ 
+	public static void main(String[] args) {
+	int x = 5;
+	int y = 5;
+	//if (x++ == 6 & ++y == 6) {/
+		//x = 11;//
+		
+	//}
+	//if(x++ ==5|++y==5) {
+		//x=11;
+	//}
+	
+	System.out.println("x="+x+",y="+y);
+	}
+	
+	
+}
