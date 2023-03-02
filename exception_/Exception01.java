750a1eaea1c392b28a5b83bf6d05bbc0bd613231
guaziba <li3424709@gmail.com>
2023-03-03 00:22:52 +0900

    异常处理练习

---
+++ exception_/Exception01.java
@@ -0,0 +1,20 @@
+package com.hspedu.exception_;
+
+public class Exception01 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+int num1 = 10;
+int num2 = 0;
+try {
+	int res = num1 / num2;
+} catch (Exception e) {
+	// TODO: handle exception
+	//e.printStackTrace();
+}
+System.out.println("程序继续进行。。");
+
+	
+	}
+
+}
